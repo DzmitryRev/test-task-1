a1ebf08d9f94cd7076919b0fcd5c0e18ee24cda6
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-09-25 15:15:58 +0300

    feat: add base structure of Slider class

---
+++ src/Slider/Slider.ts
@@ -0,0 +1,21 @@
+
+
+interface ISlide {
+
+}
+
+export class Slider {
+    slides: ISlide[]
+    currentSlide: ISlide | null;
+    constructor(slides: ISlide[]) {
+        this.slides = slides;
+        this.currentSlide = slides[0] || null;
+    }
+    nextSlide() {
+        this.currentSlide = null
+    }
+    previousSlide() {
+        this.currentSlide = null
+    }
+
+}
