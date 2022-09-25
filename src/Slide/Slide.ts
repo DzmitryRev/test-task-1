1d37d2c5013d20191cb17340a487fb8434310af3
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-09-25 15:43:05 +0300

    feat: add create template method in Slider.ts

--- src/Slide/Slide.ts
+++ src/Slide/Slide.ts
@@ -1,7 +1,19 @@
-import {ISlide} from "../Slider/Slider";
+import {ISlide} from "../index";
 
 
 export class Slide {
+    slide: ISlide;
     constructor(slide: ISlide) {
+        this.slide = slide;
+    }
+
+    createTemplate() {
+
+        const title = document.createElement("h2");
+        // add classname
+        title.innerText = this.slide.title;
+        const goal = document.createElement("p");
+        // add classname
+        goal.innerText = this.slide.goal;
     }
 }
--- src/Slider/Slider.ts
+++ src/Slider/Slider.ts
@@ -1,21 +1,31 @@
+import {Slide} from "../Slide/Slide";
+import {ISlide} from "../index";
 
-
-export interface ISlide {
-
-}
 
 export class Slider {
-    slides: ISlide[]
-    currentSlide: ISlide | null;
+    slides: Slide[]
+    currentSlide: Slide | null;
     constructor(slides: ISlide[]) {
-        this.slides = slides;
-        this.currentSlide = slides[0] || null;
+        this.slides = slides.map(slide => new Slide(slide));
+        this.currentSlide = this.slides[0] || null;
+        this.createTemplate();
     }
     nextSlide() {
         this.currentSlide = null
     }
     previousSlide() {
         this.currentSlide = null
+    }
+    createTemplate() {
+        const container = document.createElement("div");
+        container.classList.add("slider");
+        const title = document.createElement("h1");
+        title.classList.add("slider__title");
+        title.innerText = "Кейсы";
+        const info = document.createElement("div");
+        info.classList.add("slider__info")
+        const laptop = document.createElement("div");
+        laptop.classList.add("slider__laptop");
     }
 
 }
